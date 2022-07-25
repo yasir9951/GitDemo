4e9c76a401ed8497df7b2715fe381e7ce8fc33db
yasir <irfanyasir1995@gmail.com>
2022-07-25 12:14:30 +0530

    first commit

---
+++ src/demoPackage/FirstJavaProgram.java
@@ -0,0 +1,16 @@
+package demoPackage;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int a=10;
+		double b =12.34;
+		
+		String d= "car";
+		char e= 'd';
+System.out.println(a+"  "+b);		
+	}
+
+}
